8bd4fee16a93741e9b4505355ee3997b2517f985
max.ke <max.ke@hy-tech.com.tw>
2021-02-19 15:01:35 +0800

    init2

---
+++ ctbc_prototype/Scripts/js/dialog.js
@@ -0,0 +1,104 @@
+﻿var modalAlertBox = modalAlertBox || (function () {
+
+    function setTitle(title) {
+
+        $('#modalAlertBoxLabel').text(title);
+    }
+
+    function setContent(content) {
+
+        $('#errorMessage').text(content);
+    }
+
+    function setOkEvent(eventOk) {
+        $('#confirmBtn').off('click');
+        $('#confirmBtn').click(eventOk);
+    }
+
+    function setHideEvent(event) {
+        $('#modalAlertBox').off('hidden.bs.modal');
+        $('#modalAlertBox').on('hidden.bs.modal', event);
+    }
+
+    function openContainer() {
+
+        $('#modalAlertBox').modal('show');
+    }
+
+    function closeContainer() {
+        $('#modalAlertBox').modal('hide');
+    }
+
+    return {
+        open: function (title, content, hideEvent, btnOKEvent) {
+
+            setTitle(title);
+
+            setContent(content);
+
+            setHideEvent(hideEvent);
+
+            setOkEvent(btnOKEvent);           
+
+            openContainer();
+        },
+        close: closeContainer,
+    };
+
+})();
+
+var modalDialogBox = modalDialogBox || (function () {
+
+    function setTitle(title) {
+
+        $('#modalDialogBoxLabel').text(title);
+    }
+
+    function setContent(content) {
+
+        $('#dialogErrorMessage').text(content);
+    }
+
+    function openContainer() {
+
+        $('#modalDialogBox').modal('show');
+    }
+
+    function closeContainer() {
+        $('#modalDialogBox').modal('hide');
+    }
+
+    function setHideEvent(event) {
+        $('#modalDialogBox').on('hidden.bs.modal', event);
+    }
+
+    function setOkEvent(eventOk) {
+        $('#btnDialogOk').off('click');
+        $('#btnDialogOk').click(eventOk);
+    }
+
+    function setCancelEvent(eventCancel) {
+        $('#btnDialogCancel').off('click');
+        $('#btnDialogCancel').click(eventCancel);
+    }
+
+    return {
+        open: function (title, content, hideEvent, btnOKEvent , btnCancelEvent) {
+
+            setTitle(title);
+
+            setContent(content);
+
+            setHideEvent(hideEvent);
+
+            setOkEvent(btnOKEvent);
+
+            setCancelEvent(btnCancelEvent);
+
+            openContainer();
+        },
+        close: closeContainer,
+
+    };
+
+})();
